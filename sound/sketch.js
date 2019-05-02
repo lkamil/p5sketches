461010804ac1295069c5e477c1bbdc7fddc5577a
lkamil <lkamil@protonmail.com>
2019-05-02 22:17:07 +0200

    amplitude analysis

--- sound/sketch.js
+++ sound/sketch.js
@@ -1,25 +1,32 @@
 let song;
 let button;
 let jumpButton;
+let amp;
+let xoff;
 
 function setup() {
 	createCanvas(300, 300);
 
-	background(170);
-	song = loadSound("libertango.mp3");
+	background(255);
+	song = loadSound("libertango.mp3", loaded);
+	amp = new p5.Amplitude(); // this listens to the sound of the sketch
 
+	// cue is a great function to change the animation at a certain point of time
+	// song.addCue(2.0, changeBackground, color(100,100,255)); // after 2 seconds, change background, third argument gets passed to changeBackground()
+	// song.addCue(4.0, changeBackground, color(100,200,255));
+	// song.addCue(6.0, changeBackground, color(200,200,255));
+	// song.addCue(6.0, changeBackground, color(200,100,255));
+
+	xoff = 0.0
+
+}
+
+function loaded() {
 	button = createButton("play");
 	button.mousePressed(togglePlaying);
 
 	jumpButton = createButton("jump");
 	jumpButton.mousePressed(jump);
-
-	// cue is a great function to change the animation at a certain point of time
-	song.addCue(2.0, changeBackground, color(100,100,255)); // after 2 seconds, change background, third argument gets passed to changeBackground()
-	song.addCue(4.0, changeBackground, color(100,200,255));
-	song.addCue(6.0, changeBackground, color(200,200,255));
-	song.addCue(6.0, changeBackground, color(200,255,255));
-
 }
 
 function changeBackground(col) {
@@ -46,7 +53,19 @@ function jump() {
 
 
 function draw() {
-	// if (song.currentTime() > 0) {
-	//background(song.currentTime()* 2, song.currentTime(), 230);
-	// }
+	//background(200,100,255);
+	let r = noise(xoff) * 50 + 200;
+	let g = noise(xoff + 200) * 150 +100;
+	let b = noise(xoff + 500) * 200 + 50;
+
+	fill(r, g, b, 10);
+	noStroke();
+	let vol = amp.getLevel();
+	let diam = map(vol, 0, 0.2, 50, 200)
+	ellipse(width / 2, height / 2, diam, diam);
+
+	xoff += 3;
 }
+
+
+
