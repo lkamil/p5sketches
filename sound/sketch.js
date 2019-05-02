56870fc4b99979ae8bc248086b2388203eaa6cc4
lkamil <lkamil@protonmail.com>
2019-05-02 21:38:29 +0200

    timing, jumps and cues

--- sound/sketch.js
+++ sound/sketch.js
@@ -1,15 +1,29 @@
 let song;
 let button;
+let jumpButton;
 
 function setup() {
 	createCanvas(300, 300);
 
 	background(170);
-	song = loadSound("libertango.mp3"); 
+	song = loadSound("libertango.mp3");
+
 	button = createButton("play");
 	button.mousePressed(togglePlaying);
-	// song.play(); // song can be played here if it was loaded in preload
-	// song.setVolume(0.5); // Plays the song at half of it's volume
+
+	jumpButton = createButton("jump");
+	jumpButton.mousePressed(jump);
+
+	// cue is a great function to change the animation at a certain point of time
+	song.addCue(2.0, changeBackground, color(100,100,255)); // after 2 seconds, change background, third argument gets passed to changeBackground()
+	song.addCue(4.0, changeBackground, color(100,200,255));
+	song.addCue(6.0, changeBackground, color(200,200,255));
+	song.addCue(6.0, changeBackground, color(200,255,255));
+
+}
+
+function changeBackground(col) {
+	background(col);
 }
 
 function togglePlaying() {
@@ -23,5 +37,16 @@ function togglePlaying() {
 	
 }
 
+function jump() {
+	let len = song.duration(); // Gets duration of the song in seconds
+	let t = 0;// random(len);
+	console.log(t);
+	song.jump(t); // jump to the middle of the song
+}
+
+
 function draw() {
+	// if (song.currentTime() > 0) {
+	//background(song.currentTime()* 2, song.currentTime(), 230);
+	// }
 }
